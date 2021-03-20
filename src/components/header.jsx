42ecf53d0283288060f0fdec0a8874abd2cc9abb
jair <jair@hotmail.com>
2021-03-20 17:07:17 -0600

    se realizan cambios en home

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -3,7 +3,6 @@ import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { logoutRequest } from '../actions'
 import '../assets/styles/components/header.scss'
-import logo from '../assets/statics/logo-platzi-video-BW2.png'
 import userIcon from '../assets/statics/user-icon.png'
 
 const Header = props => {
@@ -17,29 +16,28 @@ const Header = props => {
   }
 
   return (
-    <header className='header'>
-
-      <Link to='/'>
-        <img className='header__img' src={logo} alt='Platzi Video' />
-      </Link>
-      <div className='header__menu'>
-        <div className='header__menu--profile'>
-          <img src={userIcon} alt='' />
-          <p>Perfil</p>
-        </div>
-        <ul>
-          {hasUser
-            ? <li><a href='/'>{user.name}</a></li>
-            : null}
-          {hasUser
-            ? <li><a href='#logout' onClick={handleLogout}>Cerrar Sesion</a></li>
-            : <li>
-              <Link to='/login'>
-                Iniciar Sesion
-              </Link>
-            </li>}
-        </ul>
+    <header>
+      <div>
+        <a href='' alt='logo'>logo-icon</a>
       </div>
+      <nav className='header__menu'>
+        <ul className='header-right'>
+          <li>
+            <img src={userIcon} alt='' />
+          </li>
+          <li className='perfil-menu'>
+            <p>Perfil</p>
+            <ul>
+              {hasUser
+                ? <li><a href='/'>{user.name}</a></li>
+                : null}
+              {hasUser
+                ? <li><a href='#logout' onClick={handleLogout}>Cerrar Sesion</a></li>
+                : <li><Link to='/login'>Iniciar Sesion</Link></li>}
+            </ul>
+          </li>
+        </ul>
+      </nav>
     </header>
   )
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import {Link } from 'react-router-dom'
+import '../assets/styles/components/home.css'
+import mainfondo from '../assets/statics/main-image.jpg'
+
+class Home extends React.Component {
+  render () {
+    return (
+      <main>
+        <section className='main-image'>
+          <img src={mainfondo} alt='fondo' />
+        </section>
+        <section className='text-main'>
+          <p>Bienvenidos <br /> FAST FOOD</p>
+        </section>
+        <section className='main-buttons'>
+          <div>
+            <Link to='/login'>
+              <button>Login</button>
+            </Link>
+          </div>
+          <div>
+            <Link to='/register'>
+              <button>Register</button>
+            </Link>
+          </div>
+        </section>
+      </main>
+    )
+  }
+}
+export default Home
